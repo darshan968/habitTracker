871001547f7cbc91ed93bc9ec5aa818d66225e76
dudedada29 <dudedada29@gmail.com>
2025-03-10 20:32:35 +0530

    new addition of files
    first commit

---
+++ index.js
@@ -0,0 +1,13 @@
+import express from "express";
+import ejs from "ejs";
+const app=express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static("public"));
+app.set("view engine", "ejs");
+app.set("views", "public");
+app.get("/", (req, res) => {
+    res.render("index.ejs", { title: "Habit Tracker App" });
+});
+
+export default app;
